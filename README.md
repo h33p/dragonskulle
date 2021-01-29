c402ddfbb615499934be1cf9539f9797f1abbc31
Aurimas Blažulionis <0x60@pm.me>
2021-01-29 13:30:08 +0000

    Initial GLFW window

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Important maven targets
+
+* `spotless:check` - checks whether the code is formatted properly
+* `spotless:apply` - formats the code
--- renderer/src/main/java/org/dragonskulle/App.java
+++ renderer/src/main/java/org/dragonskulle/App.java
@@ -1,13 +1,57 @@
+/* (C) 2021 DragonSkulle */
 package org.dragonskulle;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+/** Hello world! */
+public class App {
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+
+    private long window;
+
+    /** Entrypoint of the app instance */
+    public void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+    /** Creates a GLFW window */
+    private void initWindow() {
+        if (!glfwInit()) {
+            throw new RuntimeException("Cannot initialize GLFW");
+        }
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "CS:J", NULL, NULL);
+
+        if (window == NULL) {
+            throw new RuntimeException("Cannot create window");
+        }
+    }
+
+    private void initVulkan() {}
+
+    private void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+
+    private void cleanup() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    /** Entrypoint of the program. Creates and runs one app instance */
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
     }
 }
