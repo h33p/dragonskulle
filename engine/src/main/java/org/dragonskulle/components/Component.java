0a9a087cb40a1e7f2ee0bebf8e55de2bdbb616e6
Harry Stoltz <hbstoltz7@gmail.com>
2021-02-13 13:21:16 +0000

    Implement the abstract Component class
    Signed-off-by: Harry Stoltz <hbstoltz7@gmail.com>

--- engine/src/main/java/org/dragonskulle/components/Component.java
+++ engine/src/main/java/org/dragonskulle/components/Component.java
@@ -1,16 +1,52 @@
+/* (C) 2021 DragonSkulle */
 package org.dragonskulle.components;
 
 import org.dragonskulle.core.GameObject;
 
-public class Component {
+/**
+ * Abstract class for a Component
+ *
+ * @author Harry Stoltz
+ *      <p>
+ *          All components must extend this class, and can also optionally implement any of the
+ *          componet interfaces.
+ *          The destroy method should be overriden to handle the cleanup of any user-defined
+ *          variables on a component
+ *      </p>
+ */
+public abstract class Component {
 
     private GameObject mGameObject;
     private boolean mEnabled;
 
+    /**
+     * Destroy a component, override to handle destroying of specific components
+     */
     public void destroy() {
-        // TODO: Implement Component destroy
+        mGameObject = null;
     }
 
-
+    /**
+     * Getter for mGameObject
+     * @return mGameObject
+     */
     public GameObject getGameObject() { return mGameObject; }
+
+    /**
+     * Setter for mGameObject
+     * @param object New value of mGameObject
+     */
+    public void setGameObject(GameObject object) { mGameObject = object; }
+
+    /**
+     * Getter for mEnabled
+     * @return mEnabled
+     */
+    public boolean getEnabled() { return mEnabled; }
+
+    /**
+     * Setter for mEnabled
+     * @param value New value of mEnabled
+     */
+    public void setEnabled(boolean value) { mEnabled = value; }
 }
