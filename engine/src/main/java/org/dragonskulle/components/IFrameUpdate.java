1988b223f78a4941cdc1dd9ae39f5dc24ccf5378
Harry Stoltz <hbstoltz7@gmail.com>
2021-02-13 13:19:49 +0000

    Add all component interfaces
    Signed-off-by: Harry Stoltz <hbstoltz7@gmail.com>

---
+++ engine/src/main/java/org/dragonskulle/components/IFrameUpdate.java
@@ -0,0 +1,20 @@
+/* (C) 2021 DragonSkulle */
+package org.dragonskulle.components;
+
+/**
+ * IFrameUpdate interface
+ *
+ * @author Harry Stoltz
+ *      <p>
+ *          One of the optional interfaces for a component. Has the single method frameUpdate.
+ *      </p>
+ */
+public interface IFrameUpdate {
+
+    /**
+     * Frame Update is called every single render frame, before any fixed updates. There can be
+     * multiple, or none, fixed updates between calls to frameUpdate.
+     * @param deltaTime Approximate time since last call to frameUpdate
+     */
+    void frameUpdate(double deltaTime);
+}
---
+++ engine/src/main/java/org/dragonskulle/components/IOnAwake.java
@@ -0,0 +1,18 @@
+/* (C) 2021 DragonSkulle */
+package org.dragonskulle.components;
+
+/**
+ * IOnAwake interface
+ *
+ * @author Harry Stoltz
+ *      <p>
+ *          One of the optional interfaces for a component. Has the single method onAwake.
+ *      </p>
+ */
+public interface IOnAwake {
+
+    /**
+     * Called when a component is first added to a scene to allow initial setup of variables
+     */
+    void onAwake();
+}
