50485e27b3886589b368723c46587e71c8f16733
craig <craigwilbourne@zoho.com>
2021-02-15 16:00:53 +0000

    Added the use of user-defined actions rather than in-built key codes.
    Initial implementation of allowing user-defined actions to be triggered
    by specific keys.

---
+++ engine/src/main/java/org/dragonskulle/input/Action.java
@@ -0,0 +1,10 @@
+package org.dragonskulle.input;
+
+public enum Action {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	ACTION_1,
+	ACTION_2
+}
--- engine/src/main/java/org/dragonskulle/input/Input.java
+++ engine/src/main/java/org/dragonskulle/input/Input.java
@@ -1,5 +1,8 @@
 package org.dragonskulle.input;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,6 +21,23 @@ public class Input {
 	
 	public static final Logger LOGGER = Logger.getLogger("input");
 	
+	/** Stores whether keyboard and mouse buttons are pressed or released.*/
+	private static final HashMap<Integer, Boolean> buttons = new HashMap<Integer, Boolean>();
+	
+	
+	/** Stores whether a specific {@link Action} is being activated. */
+	private static final HashMap<Action, Boolean> actions = new HashMap<Action, Boolean>();
+	
+	/** Key: Button <br/>
+	 *  Value: {@link Action}s the Button activates.
+	 * */
+	private static final HashMap<Integer, ArrayList<Action>> buttonToAction = new HashMap<Integer, ArrayList<Action>>();
+	
+	/** Key: {@link Action} <br/>
+	 *  Value: Buttons that activate the Action.
+	 * */
+	private static final HashMap<Action, ArrayList<Integer>> actionToButton = new HashMap<Action, ArrayList<Integer>>();
+	
 	private long window;
 	
 	public Input(long window) {
@@ -26,6 +46,30 @@ public class Input {
 		this.window = window;
 		
 		LOGGER.log(Level.INFO, "Window: " + window);
+		
+
+		buttonToAction.put(265, getActionList(Action.UP, Action.ACTION_1));
+		buttonToAction.put(87, getActionList(Action.UP));
+		
+		
+		for (Entry<Integer, ArrayList<Action>> entry : buttonToAction.entrySet()) {
+			//actionToButton.put(entry.getValue(), entry.getKey());
+			for (Action action : entry.getValue()) {
+				ArrayList<Integer> buttonsList = new ArrayList<Integer>();
+				
+				buttonsList.add(entry.getKey());
+				if(actionToButton.containsKey(action)) {
+					buttonsList.addAll(actionToButton.get(action));
+				}
+				
+				actionToButton.put(action, buttonsList);
+			}
+		}
+		
+		LOGGER.info(buttonToAction.toString());
+		LOGGER.info(actionToButton.toString());
+		
+		
 		
 		/*
 		 * key: e.g. glfw.GLFW_KEY_W
@@ -37,8 +81,57 @@ public class Input {
 			
 			@Override
 			public void invoke(long window, int key, int scancode, int action, int mods) {
-				System.out.println("Key pressed.");
-				System.out.println(String.format("key: %d\nscancode: %d\naction: %d\nmods: %d", key, scancode, action, mods));
+				//LOGGER.info(String.format("key: %d\nscancode: %d\naction: %d\nmods: %d", key, scancode, action, mods));
+				
+				if(action == GLFW.GLFW_PRESS) {
+					buttons.put(key, true);
+					
+					if(buttonToAction.containsKey(key)) {
+						ArrayList<Action> selectedActions = buttonToAction.get(key);
+						
+						for (Action selectedAction : selectedActions) {
+							actions.put(selectedAction, true);
+						}						
+					}
+					
+				} else if(action == GLFW.GLFW_RELEASE) {
+					buttons.put(key, false);
+					
+					if(buttonToAction.containsKey(key)) {
+						ArrayList<Action> selectedActions = buttonToAction.get(key);
+						
+						for (Action selectedAction : selectedActions) {
+							ArrayList<Integer> buttonsCauseAction = actionToButton.get(selectedAction);
+							if(buttonsCauseAction == null) {
+								LOGGER.severe("buttonsCauseAction is null!");
+								return;
+							}
+							
+							Boolean remove = true;
+							for(Integer selectedButton : buttonsCauseAction) {
+								if(buttons.containsKey(selectedButton) && buttons.get(selectedButton) == true) {
+									// Another button is currently activated, so do not set action to false.
+									remove = false;
+									break;
+								}
+							}
+							
+							if(remove) {
+								actions.put(selectedAction, false);
+							}
+						}
+						
+						/*
+						ArrayList<Action> selectedActions = buttonToAction.get(key);
+						
+						for (Action selectedAction : selectedActions) {
+							actions.put(selectedAction, false);
+						}
+						*/						
+					}
+				}
+				
+				LOGGER.info(actions.toString());
 			}
 		};
 		
@@ -46,8 +139,15 @@ public class Input {
 			
 			@Override
 			public void invoke(long window, int button, int action, int mods) {
-				System.out.println("Mouse button pressed.");
-				System.out.println(String.format("button: %d\naction: %d\nmods: %d", button, action, mods));
+				//System.out.println(String.format("button: %d\naction: %d\nmods: %d", button, action, mods));
+				
+				if(action == GLFW.GLFW_PRESS) {
+					buttons.put(button, true);
+				} else if(action == GLFW.GLFW_RELEASE) {
+					buttons.put(button, false);
+				}
+				
+				LOGGER.info(actions.toString());
 			}
 		};
 		
@@ -55,8 +155,8 @@ public class Input {
 			
 			@Override
 			public void invoke(long window, double xpos, double ypos) {
-				// TODO Auto-generated method stub
-				
+				//System.out.println("Mouse position.");
+				//System.out.println(String.format("xpos: %f\nypos: %f", xpos, ypos));
 			}
 		};
 		
@@ -64,8 +164,8 @@ public class Input {
 			
 			@Override
 			public void invoke(long window, double xoffset, double yoffset) {
-				// TODO Auto-generated method stub
-				
+				//System.out.println("Mouse scroll.");
+				//System.out.println(String.format("xoffset: %f\nyoffset: %f", xoffset, yoffset));
 			}
 		};
 		
@@ -74,7 +174,20 @@ public class Input {
 		GLFW.glfwSetCursorPosCallback(window, mousePosition);
 		GLFW.glfwSetScrollCallback(window, scroll);
 		
-		//glfwSetKeyCallback(window, keyboard);
+		// For infinite mouse movement.
+		// GLFW.glfwSetInputMode(window, GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_DISABLED);
+		// For hiding the cursor.
+		// GLFW.glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+	}
+	
+	private ArrayList<Action> getActionList(Action... selectedActions){
+		ArrayList<Action> list = new ArrayList<Action>();
+		
+		for (Action action : selectedActions) {
+			list.add(action);
+		}
+		
+		return list;
 	}
 	
 }
