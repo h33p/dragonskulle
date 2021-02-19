d15c03850cb52d5d3624bfe17542b0d58b124d5c
Aurimas Blažulionis <0x60@pm.me>
2021-02-19 13:44:22 +0000

    Add missed layout transition

--- engine/src/main/java/org/dragonskulle/renderer/RenderedApp.java
+++ engine/src/main/java/org/dragonskulle/renderer/RenderedApp.java
@@ -1549,8 +1549,15 @@ public class RenderedApp {
                         VK_FORMAT_R8G8B8A8_SRGB,
                         VK_IMAGE_LAYOUT_UNDEFINED,
                         VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+
                 copyBufferToImage(
                         stagingBuffer, textureImage, texture.getWidth(), texture.getHeight());
+
+                transitionImageLayout(
+                        textureImage.image,
+                        VK_FORMAT_R8G8B8A8_SRGB,
+                        VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                        VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
                 stagingBuffer.destroyBuffer(device);
             }
