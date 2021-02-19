4a42536b34a65fd6f25630ce22aeab31c4e9a1d0
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-02-19 11:26:37 +0000

    Added Basic JavaDocs

--- engine/src/main/java/org/dragonskulle/audio/AudioManager.java
+++ engine/src/main/java/org/dragonskulle/audio/AudioManager.java
@@ -8,27 +8,56 @@ package org.dragonskulle.audio;
  */
 public class AudioManager {
 	
+	/**
+	 * Will play some audio
+	 * @param channel Whether to play as Background music or as a Sound Effect
+	 * @param numberId The audio number to play
+	 * @param distance How far from the source the camera is.  Use null to ignore
+	 */
 	public void play(SoundType channel, int numberId, Integer distance)
 	{
 		;
 	}
 	
+	/**
+	 * Set the mute value for a specific channel
+	 * @param channel The channel to mute
+	 * @param muteValue whether to mute the channel or not
+	 */
 	public void setMute(SoundType channel, boolean muteValue) {
 		;
 	}
 	
+	/**
+	 * Get the current mute value of the selected channel 
+	 * @param channel The channel to check 
+	 * @return whether the channel is muted
+	 */
 	public boolean getMute(SoundType channel) {
 		return true;
 	}
 	
+	/**  
+	 * Set the volume of selected channel between 0 and 100
+	 * @param channel The channel to change volume
+	 * @param setVol the volume to change to
+	 */
 	public void setVolume(SoundType channel, int setVol) {
 		;
 	}
 	
+	/**
+	 * Get the current volume of selected channel
+	 * @param channel The channel to select
+	 * @return The current volume
+	 */
 	public int getVolume(SoundType channel) {
 		return -1;
 	}
 	
+	/**
+	 * Destroys all Clips.   Must be called at end of program
+	 */
 	public void cleanup()
 	{
 		;
--- engine/src/main/java/org/dragonskulle/audio/DataLinePool.java
+++ engine/src/main/java/org/dragonskulle/audio/DataLinePool.java
@@ -12,19 +12,36 @@ import javax.sound.sampled.DataLine;
  */
 public class DataLinePool {
 
-	public Clip openStream(AudioInputStream inputs) {
+	/**
+	 * Play a sound effect
+	 * @param input The stream to be played
+	 * @return the {@code Clip} which has been played on
+	 */
+	public Clip openStream(AudioInputStream input) {
 		return null;
 	}
 	
+	/**
+	 * Set the mute value
+	 * @param setMute the {@code boolean} value to set mute
+	 */
 	public void setMute(boolean setMute) {
 		;
 	}
 	
-	public void setVolume(boolean setVol) {
+	/**
+	 * Set the volume
+	 * @param setVol an {@code int} value between 0 and 100 inclusive
+	 */
+	public void setVolume(int setVol) {
 		;
 	}
 	
-	public DataLine.Info[] cleanup(){
+	/**
+	 * Will return an array of clips which have been used so they can removed
+	 * @return An {@code array} of {@code Clips} 
+	 */
+	public Clip[] cleanup(){
 		return null;
 	}
 }
