116837cd75469d9ed990ba67f5a6d311d38c6948
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-02-23 16:48:15 +0000

    Formatted AudioSource

--- engine/src/main/java/org/dragonskulle/audio/AudioSource.java
+++ engine/src/main/java/org/dragonskulle/audio/AudioSource.java
@@ -1,30 +1,26 @@
+/* (C) 2021 DragonSkulle */
 package org.dragonskulle.audio;
 
 import org.dragonskulle.components.Component;
 
 /**
- * An engine component which allows you to play audio.   
- * @author Dragonskulle
- *  <p> Both filename and the SoundType channel are public fields which can be accessed </p>
+ * An engine component which allows you to play audio.
  *
+ * @author Dragonskulle
+ *     <p>Both filename and the SoundType channel are public fields which can be accessed
  */
 public class AudioSource extends Component {
 
-	
-	public String filename;  
-	public SoundType channel = SoundType.SFX;
-	
-	/**
-	 * Constructor
-	 */
-	public AudioSource() {
-		;
-	}
-	
-	/**
-	 * Plays the audio on the current specified channel from the current specified filename
-	 */
-	public void play() {
-		AudioManager.getInstance().play(channel, filename);
-	}
+    public String filename;
+    public SoundType channel = SoundType.SFX;
+
+    /** Constructor */
+    public AudioSource() {
+        ;
+    }
+
+    /** Plays the audio on the current specified channel from the current specified filename */
+    public void play() {
+        AudioManager.getInstance().play(channel, filename);
+    }
 }
