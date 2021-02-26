a02fa7c1d7e434c41f0b4a95d942c4e12895d21a
Harry Stoltz <hbstoltz7@gmail.com>
2021-02-26 13:39:40 +0000

    Fix formatting
    Signed-off-by: Harry Stoltz <hbstoltz7@gmail.com>

--- game/src/main/java/org/dragonskulle/game/App.java
+++ game/src/main/java/org/dragonskulle/game/App.java
@@ -1,9 +1,6 @@
 /* (C) 2021 DragonSkulle */
 package org.dragonskulle.game;
 
-import org.dragonskulle.core.Engine;
-import org.dragonskulle.core.GameObject;
-import org.dragonskulle.core.Scene;
 import org.dragonskulle.renderer.RenderedApp;
 
 public class App extends RenderedApp {
@@ -12,21 +9,8 @@ public class App extends RenderedApp {
 
     /** Entrypoint of the program. Creates and runs one app instance */
     public static void main(String[] args) {
-        Scene scene = new Scene("Main");
-
-        GameObject root = new GameObject("root");
-
-        GameObject obj1 = new GameObject("Object 1");
-        GameObject obj2 = new GameObject("Object 2");
-
-        obj1.addComponent(new ExampleComponent(1));
-        obj2.addComponent(new ExampleComponent(2));
-
-        obj1.addChild(obj2);
-        root.addChild(obj1);
-
-        scene.addRootObject(root);
-
-        Engine.getInstance().start(scene);
+        App app = new App();
+        app.run(WIDTH, HEIGHT, "Germany");
+        System.out.println(app.getWindow());
     }
 }
