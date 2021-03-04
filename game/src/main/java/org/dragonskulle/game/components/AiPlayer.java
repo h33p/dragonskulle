1d1ca44fa30b5fd0f0eafb967b379069d6ea3ba6
Oscar Lindenbaum - 2005912 <oxl912@cs.bham.ac.uk>
2021-03-04 14:04:13 +0000

    created ai and human instances of player

---
+++ game/src/main/java/org/dragonskulle/game/components/AiPlayer.java
@@ -0,0 +1,22 @@
+package org.dragonskulle.game.components;
+
+import org.dragonskulle.components.IFixedUpdate;
+
+/**
+ * @author Oscar L
+ */
+public class AiPlayer extends Player implements IFixedUpdate {
+    @Override
+    protected void onDestroy() {
+
+    }
+
+    @Override
+    public void fixedUpdate(float deltaTime) {
+
+    }
+
+    public void simulateInput(){
+
+    }
+}
---
+++ game/src/main/java/org/dragonskulle/game/components/HumanPlayer.java
@@ -0,0 +1,21 @@
+package org.dragonskulle.game.components;
+
+import org.dragonskulle.components.IFrameUpdate;
+
+/**
+ * @author Harry Stoltz, Oscar Lindenbaum and Nathaniel Lowis
+ */
+public class HumanPlayer extends Player implements IFrameUpdate {
+
+
+    @Override
+    protected void onDestroy() {
+
+    }
+
+    @Override
+    public void frameUpdate(float deltaTime) {
+
+    }
+
+}
