d0944d48d9b9e07f0fe745c6f2522c3936f9833d
Harry Stoltz <hbstoltz7@gmail.com>
2021-03-04 13:52:44 +0000

    Add abstract Player Class
    Signed-off-by: Harry Stoltz <hbstoltz7@gmail.com>

---
+++ game/src/main/java/org/dragonskulle/game/components/Player.java
@@ -0,0 +1,11 @@
+package org.dragonskulle.game.components;
+
+import org.dragonskulle.components.Component;
+
+/**
+ * Abstract Player class, Both AIPlayer and HumanPlayer will extend this
+ *
+ * @author Harry Stoltz, Oscar Lindenbaum and Nathaniel Lowis
+ */
+abstract public class Player extends Component {
+}
