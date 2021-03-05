2a0b239eb8c081be952ca1485b22f31665f953bf
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-03-05 19:58:29 +0000

    Added a way to check if AI player should have a go (which is random)

--- game/src/main/java/org/dragonskulle/game/components/AiPlayer.java
+++ game/src/main/java/org/dragonskulle/game/components/AiPlayer.java
@@ -1,5 +1,7 @@
 package org.dragonskulle.game.components;
 
+import java.util.Random;
+
 import org.dragonskulle.components.IFixedUpdate;
 
 /**
@@ -7,14 +9,64 @@ import org.dragonskulle.components.IFixedUpdate;
  * @author Oscar L
  */
 public class AiPlayer extends Player implements IFixedUpdate {
-    @Override
+    
+	protected Random random;
+	protected float timeSinceStart;
+	protected int lowerBoundTime;
+	protected int upperBoundTime;
+	protected int actualTime;
+	
+	
+	public AiPlayer(int lowerBound, int upperBound) {
+		
+		//this.super();
+		lowerBoundTime = lowerBound;
+		upperBoundTime = upperBound;
+		
+		createNewRandomTime();
+		
+		
+	}
+	
+	/**
+	 * This will check to see whether the AI Player can actually play or not
+	 * @param deltaTime The time since the last fixed update
+	 * @return A boolean to say whether the AI player can play
+	 */
+	protected boolean playGame(float deltaTime) {
+		timeSinceStart += deltaTime;
+		
+		if (timeSinceStart >= actualTime) {
+			timeSinceStart = 0;
+			createNewRandomTime();
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * This will set how long the AI player has to wait until they can play
+	 */
+	protected void createNewRandomTime() {
+		do {
+			
+			actualTime = random.nextInt(upperBoundTime+1);
+		} while (actualTime < lowerBoundTime);
+	}
+	
+	@Override
     protected void onDestroy() {
 
     }
 
     @Override
     public void fixedUpdate(float deltaTime) {
-
+    	if (playGame(deltaTime)) {
+    		simulateInput();
+    		triggerEvent();
+    		
+    	}
     }
 
     /**
