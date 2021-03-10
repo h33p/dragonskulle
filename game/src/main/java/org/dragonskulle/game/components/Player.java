ba8f48571383abac428408ef76c6c5951b21e4c5
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-03-10 12:12:25 +0000

    Updated token Update

--- game/src/main/java/org/dragonskulle/game/components/Player.java
+++ game/src/main/java/org/dragonskulle/game/components/Player.java
@@ -16,7 +16,8 @@ abstract public class Player extends Component {
 	protected int tokens;  //NEED TO BE SYNCED
 	protected final int TOKEN_RATE = 5;
 	protected final float UPDATE_TIME = 1;
-	protected float lastTokenUpdate = Time.getTimeInSeconds() - UPDATE_TIME;
+	protected float lastTokenUpdate = 0;
+	protected Reference<HexMap> mapComponent;
 	
 	/**
 	 * This method will take the action decided by the user and will tell the server to perform one action 
@@ -28,16 +29,16 @@ abstract public class Player extends Component {
 	/**
 	 * This method will update the amount of tokens the user has per UPDATE_TIME.  Goes through all owned buildings to check if need to update tokens
 	 */
-	protected void updateTokens() {
-		float time = Time.getTimeInSeconds();
-		
-		if (time - lastTokenUpdate > UPDATE_TIME) {
+	protected void updateTokens(float time) {
+				
+		lastTokenUpdate += time;
+		if (time > UPDATE_TIME) {
 			for (Building building: ownedBuildings) {
 				tokens += building.getToken();
 				
 			}
 			tokens += TOKEN_RATE;
-			lastTokenUpdate = time;
+			lastTokenUpdate = 0;
 		}
 	}
 }
