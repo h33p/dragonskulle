b2ad3f98c76d32a7dc9b0029b1d24ff0610f53da
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-03-10 12:12:59 +0000

    Started implementing what to do when user inputs

--- game/src/main/java/org/dragonskulle/game/components/HumanPlayer.java
+++ game/src/main/java/org/dragonskulle/game/components/HumanPlayer.java
@@ -8,22 +8,36 @@ import org.dragonskulle.components.IFrameUpdate;
  */
 public class HumanPlayer extends Player implements IFrameUpdate {
 
+	private Screen screenOn = Screen.MAP_SCREEN;
 
     @Override
     protected void onDestroy() {
-
+    	
     }
 
     @Override
     public void frameUpdate(float deltaTime) {
-
+    	
     }
     
     /**
      * This will take an input from the User and will then change this into a way for the class to understand what the player wants to do
      */
     private void processInput() {
-    	
+    	if (screenOn == Screen.MAP_SCREEN) {
+    		mapScreen();
+    	}
+    	else if (screenOn == Screen.BUILDING_SCREEN){
+    		buildingScreen();
+    	}
+    }
+    
+    private void mapScreen() {
+    	;//Need way to check if click hits tile
+    }
+    
+    private void buildingScreen() {
+    	;
     }
 
 }
