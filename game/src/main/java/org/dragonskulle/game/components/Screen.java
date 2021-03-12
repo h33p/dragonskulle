1b1dd24058ef650214b26ee93ed4c029116b54b7
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-03-12 16:29:24 +0000

    Added Comments

--- game/src/main/java/org/dragonskulle/game/components/Screen.java
+++ game/src/main/java/org/dragonskulle/game/components/Screen.java
@@ -2,7 +2,7 @@ package org.dragonskulle.game.components;
 
 public enum Screen {
 	
-	MAP_SCREEN,
-	BUILDING_SCREEN,
-	TILE_SCREEN
+	MAP_SCREEN,			//All the buildings you can see everything need to click a tile to interact 
+	BUILDING_SCREEN,	//The actions you can take when you have clicked on your building
+	TILE_SCREEN			//The action to build a building on that tile.
 }
