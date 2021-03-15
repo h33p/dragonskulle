e8e1cf5e419c4ecfee3a6f69e6c85f43a599d515
craig-r-w <56520435+craig-r-w@users.noreply.github.com>
2021-03-15 13:41:59 +0000

    Changed the VBLANK_MODE.

--- engine/src/main/java/org/dragonskulle/renderer/SwapchainSupportDetails.java
+++ engine/src/main/java/org/dragonskulle/renderer/SwapchainSupportDetails.java
@@ -19,8 +19,8 @@ class SwapchainSupportDetails {
     IntBuffer presentModes;
     public static final Logger LOGGER = Logger.getLogger("render");
 
-    private static final int VBLANK_MODE = envInt("VBLANK_MODE", VK_PRESENT_MODE_MAILBOX_KHR);
-
+    private static final int VBLANK_MODE = envInt("VBLANK_MODE", VK_PRESENT_MODE_FIFO_KHR);
+    
     public SwapchainSupportDetails(VkPhysicalDevice device, long surface) {
         capabilities = VkSurfaceCapabilitiesKHR.create();
         vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, capabilities);
