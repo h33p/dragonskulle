1395f3449601e7a351a279899e69a9d9f89c1ecd
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-03-15 13:47:12 +0000

    Added JavaDocs and brought to ASOP standard

--- game/src/main/java/org/dragonskulle/game/audio/GameAudio.java
+++ game/src/main/java/org/dragonskulle/game/audio/GameAudio.java
@@ -4,18 +4,34 @@ import org.dragonskulle.audio.AudioSource;
 import org.dragonskulle.audio.SoundType;
 import org.dragonskulle.ui.UIButton.IButtonEvent;
 
+/**
+ * A class which allows you to add sound to button clicks
+ * @author Dragonskulle
+ *
+ */
 public class GameAudio {
 	
-	AudioSource audio;
+	private AudioSource mAudio;
+	
+	/**
+	 * The constructor 
+	 * @param filename The file which is in resources/audio
+	 * @param channel The channel to play on
+	 */
 	public GameAudio(String filename, SoundType channel) {
-		audio = new AudioSource();
-		audio.filename = filename;
-		audio.channel = channel;
+		mAudio = new AudioSource();
+		mAudio.filename = filename;
+		mAudio.channel = channel;
 	}
 	
+	/**
+	 * The button click to play the sound
+	 * @param onClick The stuff to do when clicking the button
+	 * @return The stuff to do when clicking a button + playing a sound
+	 */
 	public IButtonEvent audibleClick(IButtonEvent onClick) {
 		return (a, b) -> {
-			audio.play();
+			mAudio.play();
 			if (onClick != null) {
 				onClick.eventHandler(a, b);
 			}
