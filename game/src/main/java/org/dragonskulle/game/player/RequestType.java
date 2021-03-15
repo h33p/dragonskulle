c1df12f3ea0ea1e6166d67208eb10348f25a5f85
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-03-15 18:55:35 +0000

    Removed unneeded files

--- game/src/main/java/org/dragonskulle/game/player/RequestType.java
+++
@@ -1,7 +0,0 @@
-/* (C) 2021 DragonSkulle */
-package org.dragonskulle.game.player;
-
-public enum RequestType {
-    BUILDING, // A request to do something with a building
-    PLACE // A request to build something in that hextile
-}
