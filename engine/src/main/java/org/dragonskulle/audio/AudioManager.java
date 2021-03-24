675659ba1df0613dc01fcce96e2dd6c0cdc35508
Harry Stoltz <hbstoltz7@gmail.com>
2021-03-24 21:44:50 +0000

    Add all methods to AudioSource Add update method in AudioManager
    Signed-off-by: Harry Stoltz <hbstoltz7@gmail.com>

--- engine/src/main/java/org/dragonskulle/audio/AudioManager.java
+++ engine/src/main/java/org/dragonskulle/audio/AudioManager.java
@@ -1,5 +1,6 @@
 package org.dragonskulle.audio;
 
+import org.dragonskulle.audio.components.AudioListener;
 import org.dragonskulle.audio.components.AudioSource;
 import org.dragonskulle.core.Reference;
 import org.lwjgl.openal.AL;
@@ -18,6 +19,7 @@ import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -39,13 +41,18 @@ public class AudioManager {
     private final Map<Integer, WaveSound> mSoundMap = new HashMap<>();
     private final ArrayList<Source> mSources = new ArrayList<>();
     private final HashSet<Reference<AudioSource>> mAudioSources = new HashSet<>();
+    private final ArrayList<Reference<AudioSource>> mActiveAudioSources = new ArrayList<>();
 
-
+    private Reference<AudioListener> mAudioListener;
     private long mALDev = -1;
     private long mALCtx = -1;
 
+    /**
+     * Constructor for AudioManager. It's private as AudioManager is designed as a singleton. Opens
+     * an OpenAL device and creates and sets the context for the process. Also attempts to create
+     * up to MAX_SOURCES sources
+     */
     private AudioManager() {
-
         // TODO: For now I'm just using the "best" device available, I have made AudioDevices.java
         //       which can be used to enumerate devices so we can allow user to choose in the future
         long device = ALC11.alcOpenDevice((ByteBuffer)null);
@@ -79,6 +86,9 @@ public class AudioManager {
                 + mSources.size() + " sources available");
     }
 
+    /**
+     * Attempt to create MAX_SOURCES sources
+     */
     private void setupSources() {
         for (int i = 0; i < MAX_SOURCES; i++) {
             int source = AL11.alGenSources();
@@ -90,6 +100,51 @@ public class AudioManager {
             s.setSource(source);
 
             mSources.add(s);
+        }
+    }
+
+    /**
+     * Attempt to get the first source in mSources that is not in use.
+     * In theory, this should never return null as if there are more than MAX_SOURCES AudioSources
+     * currently active, some will have their source detached before this is ever called
+     *
+     * @return An inactive Source, or null if none were available
+     */
+    private Source getAvailableSource() {
+        for (Source s : mSources) {
+            if (!s.isInUse()) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Attach an OpenAL source to all AudioSources in the list
+     *
+     * @param audioSources List of AudioSources to attach a source to
+     */
+    private void attachSources(List<Reference<AudioSource>> audioSources) {
+        for (Reference<AudioSource> ref : audioSources) {
+            AudioSource audioSource = ref.get();
+
+            if (audioSource.getSource() == null) {
+                Source source = getAvailableSource();
+                assert source != null;
+
+                audioSource.attachSource(source);
+            }
+        }
+    }
+
+    /**
+     * Detach the OpenAL source from each AudioSource in the list
+     *
+     * @param audioSources List of AudioSources to detach a source from
+     */
+    private void detachSources(List<Reference<AudioSource>> audioSources) {
+        for (Reference<AudioSource> ref : audioSources) {
+            ref.get().detachSource();
         }
     }
 
@@ -134,6 +189,16 @@ public class AudioManager {
     }
 
     /**
+     * Attempt to get a loaded sound by id
+     *
+     * @param id Integer of id that the sound was loaded with
+     * @return A WaveSound object representing the sound, or null if there was no sound with that id
+     */
+    public WaveSound getSound(int id) {
+        return mSoundMap.getOrDefault(id, null);
+    }
+
+    /**
      * Handles the distribution of sources between all of the AudioSources in the scene, assigning
      * sources to those that have the highest priority and removing them from those that no longer
      * need them
@@ -142,31 +207,63 @@ public class AudioManager {
         // First remove any references to AudioSources that are no longer valid
         mAudioSources.removeIf(ref -> !ref.isValid());
 
-        // TODO: Some way to sort the list of AudioSources so that higher priority is at the front
-        //      Then iterate through, detach all sources from AudioSources that are no longer in use
-        //      or are out of range etc
-        //      Then go back through attaching the free sources to the highest priority AudioSources
-        //      that currently do not have a source attached
+        mActiveAudioSources.addAll(mAudioSources);
 
+        // All references will be valid because they any invalid ones are removed at the start
+        for (int i = 0; i < mActiveAudioSources.size(); i++) {
+            AudioSource audioSource = mActiveAudioSources.get(i).get();
 
+            // TODO: Distance check
+            // TODO: Check audio time left (Need to calculate sound length first)
+
+            if (audioSource.getSound() == null) {
+                mActiveAudioSources.remove(i);
+                audioSource.detachSource();
+                i--;
+            }
+        }
+
+        // List is not sorted by priority for now but will be in the future so
+        // detach all sources from index mSources.size() onwards
+        if (mActiveAudioSources.size() > mSources.size()) {
+            detachSources(mActiveAudioSources.subList(mSources.size(), mActiveAudioSources.size()));
+            attachSources(mActiveAudioSources.subList(0, mSources.size()));
+        } else {
+            attachSources(mActiveAudioSources);
+        }
+
+        mActiveAudioSources.clear();
     }
 
     /**
-     * Register an audio source so that it can have a source attached to it when required
+     * Add an audio source so that it can have a source attached to it when required
      *
-     * @param audioSource Reference to the AudioSource that will be registered
+     * @param audioSource Reference to the AudioSource that will be added
      */
-    public void registerAudioSource(Reference<AudioSource> audioSource) {
+    public void addAudioSource(Reference<AudioSource> audioSource) {
         mAudioSources.add(audioSource);
     }
 
     /**
-     * De-register an audio source so that it will no longer have sources attached to it
+     * Remove an audio source so that it will no longer have sources attached to it
      *
-     * @param audioSource Reference to the AudioSource that will be registered
+     * @param audioSource Reference to the AudioSource that will be removed
      */
-    public void deregisterAudioSource(Reference<AudioSource> audioSource) {
+    public void removeAudioSource(Reference<AudioSource> audioSource) {
         mAudioSources.remove(audioSource);
+    }
+
+    /**
+     * Set the active AudioListener component for the scene. If multiple AudioListeners exist in one
+     * scene, there is no way to know for certain which audioListener will actually be set as the
+     * active listener and that behaviour should not be relied upon.
+     *
+     * @param audioListener Reference to the AudioListener component to be used
+     */
+    public void setAudioListener(Reference<AudioListener> audioListener) {
+        if (audioListener.isValid()) {
+            mAudioListener = audioListener;
+        }
     }
 
     /**
--- engine/src/main/java/org/dragonskulle/audio/components/AudioSource.java
+++ engine/src/main/java/org/dragonskulle/audio/components/AudioSource.java
@@ -1,26 +1,171 @@
 package org.dragonskulle.audio.components;
 
+import org.dragonskulle.audio.AudioManager;
+import org.dragonskulle.audio.Source;
+import org.dragonskulle.audio.WaveSound;
 import org.dragonskulle.components.Component;
 import org.dragonskulle.components.IFixedUpdate;
 import org.dragonskulle.components.IOnAwake;
+import org.joml.Vector3f;
+import org.lwjgl.openal.AL11;
 
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Accessors(prefix = "m")
 public class AudioSource extends Component implements IFixedUpdate, IOnAwake {
 
+    @Getter private WaveSound mSound = null;
+    @Getter private Source mSource = null;
+    @Getter private float mVolume = 1f;
+    @Getter private float mPitch = 1f;
+    @Getter private float mRadius = 10f;
+    @Getter private int mLooping = AL11.AL_FALSE;
 
+    /**
+     * Update the position of the source to that of the GameObject
+     */
+    private void updatePosition() {
+        if (mSource == null) {
+            return;
+        }
 
+        Vector3f pos = mGameObject.getTransform().getPosition();
+        AL11.alSource3f(mSource.getSource(), AL11.AL_POSITION, pos.x, pos.y, pos.z);
+    }
+
+    /**
+     * Attach a Source to this AudioSource
+     *
+     * @param source Source to be attached
+     */
+    public void attachSource(Source source) {
+        if (source == null) {
+            return;
+        }
+
+        mSource = source;
+
+        mSource.setInUse(true);
+
+        int s = mSource.getSource();
+
+        AL11.alSourcef(s, AL11.AL_GAIN, mVolume);
+        AL11.alSourcef(s, AL11.AL_PITCH, mPitch);
+        AL11.alSourcef(s, AL11.AL_MAX_DISTANCE, mRadius);
+        AL11.alSourcei(s, AL11.AL_LOOPING, mLooping);
+
+        if (mSound != null) {
+            AL11.alSourcei(s, AL11.AL_BUFFER, mSound.buffer);
+        }
+        updatePosition();
+
+        AL11.alSourcePlay(s);
+    }
+
+    /**
+     * Detach the Source from this AudioSource if there is one
+     */
+    public void detachSource() {
+        if (mSource == null) {
+            return;
+        }
+
+        int source = mSource.getSource();
+
+        AL11.alSourcef(source, AL11.AL_GAIN, 0f);
+        AL11.alSourceStop(source);
+        AL11.alSourcei(source, AL11.AL_BUFFER, 0);
+
+        mSource.setInUse(false);
+        mSource = null;
+    }
+
+    /**
+     * Set the volume of the AudioSource
+     *
+     * @param volume Volume from 0f to 1f
+     */
+    public void setVolume(float volume) {
+        volume = Math.max(0f, volume);
+        volume = Math.min(volume, 1f);
+
+        mVolume = volume;
+        if (mSource != null) {
+            AL11.alSourcef(mSource.getSource(), AL11.AL_GAIN, volume);
+        }
+    }
+
+    /**
+     * Set whether the AudioSource should loop
+     *
+     * @param looping Whether to loop or not
+     */
+    public void setLooping(boolean looping) {
+        mLooping = looping ? AL11.AL_TRUE : AL11.AL_FALSE;
+
+        if (mSource != null) {
+            AL11.alSourcei(mSource.getSource(), AL11.AL_LOOPING, mLooping);
+        }
+    }
+
+    /**
+     * Set the sound that will be played from this AudioSource
+     *
+     * @param soundID ID that the desired sound was loaded with
+     */
+    public void setSound(int soundID) {
+        WaveSound sound = AudioManager.getInstance().getSound(soundID);
+        if (sound == null) {
+            return;
+        }
+
+        mSound = sound;
+        detachSource();
+    }
+
+    /**
+     * Set the pitch of the sound
+     *
+     * @param pitch Desired pitch (Value between 0f and 1f)
+     */
+    public void setPitch(float pitch) {
+        pitch = Math.max(0f, pitch);
+        pitch = Math.min(pitch, 1f);
+
+        mPitch = pitch;
+        if (mSource != null) {
+            AL11.alSourcef(mSource.getSource(), AL11.AL_PITCH, mPitch);
+        }
+    }
+
+    /**
+     * Set the radius from the AudioSource that the sound can be heard from
+     *
+     * @param radius New radius. Must be a positive value
+     */
+    public void setRadius(float radius) {
+        radius = Math.max(0f, radius);
+
+        mRadius = radius;
+        if (mSource != null) {
+            AL11.alSourcef(mSource.getSource(), AL11.AL_MAX_DISTANCE, mRadius);
+        }
+    }
 
     @Override
     protected void onDestroy() {
-        // TODO: Stop playback and set source to not in use
+        detachSource();
+        mSound = null;
     }
 
     @Override
     public void fixedUpdate(float deltaTime) {
-        // TODO: Update source position to follow GameObject's transform
+        updatePosition();
     }
 
     @Override
     public void onAwake() {
-
+        AudioManager.getInstance().addAudioSource(getReference(AudioSource.class));
     }
 }
