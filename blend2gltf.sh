6044b2864bbc27550752ab6667632b2201422cf1
Aurimas Blažulionis <0x60@pm.me>
2021-04-02 11:24:43 +0100

    Remove textures/gltfs before rebuilding them

--- blend2gltf.sh
+++ blend2gltf.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+rm game/src/main/resources/textures/gltf/*
+rm game/src/main/resources/gltf/*
+
 for s in assets/*.blend; do
 	blender "$s" --background --enable-autoexec --python assets/gltf_export.py -- ${PWD}/game/src/main/resources/
 done
+
+rm engine/src/test/resources/textures/gltf/*
+rm engine/src/test/resources/gltf/*
 
 for s in assets/test/*.blend; do
 	blender "$s" --background --enable-autoexec --python assets/gltf_export.py -- ${PWD}/engine/src/test/resources/
