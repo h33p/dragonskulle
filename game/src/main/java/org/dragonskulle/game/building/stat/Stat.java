4d2ff2cff4c7ba99a5a64b49b9ac8423fdc523bf
craig-r-w <56520435+craig-r-w@users.noreply.github.com>
2021-04-06 20:33:32 +0100

    Added Stat IDs and reworked StatData.

--- game/src/main/java/org/dragonskulle/game/building/stat/Stat.java
+++ game/src/main/java/org/dragonskulle/game/building/stat/Stat.java
@@ -1,10 +1,53 @@
 /* (C) 2021 DragonSkulle */
 package org.dragonskulle.game.building.stat;
 
-public enum Stat {
-    ATTACK,
+import lombok.extern.java.Log;
+
+@Log
+public enum Stat {	
+	ATTACK,
     DEFENCE,
     TOKEN_GENERATION,
     VIEW_DISTANCE,
-    ATTACK_DISTANCE
+    ATTACK_DISTANCE;
+
+	/**
+	 * The index of the specific Stat in {@link #values()}.
+	 */
+	private int mID;
+	
+	/**
+	 * Set the IDs of the Stats.
+	 */
+	static {
+		int current = 0;
+		for (Stat stat : values()) {
+			stat.mID = current;
+			current++;
+		}
+	}
+	
+	/**
+	 * Get the ID of the Stat.
+	 * @return The ID.
+	 */
+	public int getID() {
+		return mID;
+	}
+	
+	/**
+	 * Get a {@link Stat} from its ID.
+	 * @param id The ID of the desired Stat.
+	 * @return The desired Stat, or {@code null}.
+	 */
+	static public Stat getFromID(int id) {
+		Stat[] values = values();
+		if(id < 0 || id > values.length) {
+			log.warning("Stat ID out of range: " + id);
+			return null;
+		}
+		
+		return values[id];
+	}
+	
 }
--- game/src/main/java/org/dragonskulle/game/player/network_data/StatData.java
+++ game/src/main/java/org/dragonskulle/game/player/network_data/StatData.java
@@ -6,51 +6,80 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import lombok.experimental.Accessors;
 import org.dragonskulle.game.building.Building;
-import org.dragonskulle.game.building.stat.SyncStat;
+import org.dragonskulle.game.building.stat.Stat;
 import org.dragonskulle.game.map.HexagonMap;
 import org.dragonskulle.game.map.HexagonTile;
 import org.dragonskulle.network.components.sync.INetSerializable;
 
 /**
- * The Class which holds the data to be sent for upgrading stats
- *
- * @author low101043
+ * Holds data to allow a {@link Building} to increase a specific {@link Stat}.
+ * 
+ * @author Craig Wilbourne
  */
 @Accessors(prefix = "m")
 public class StatData implements INetSerializable {
 
-    public SyncStat<?> mStat;
-
+    /** The ID of the {@link Stat} to change. */
+    private int mStatID;
+    /** The q coordinate of the desired {@link Building}. */
     private int mQ;
+    /** The r coordinate of the desired {@link Building}. */
     private int mR;
 
     public StatData() {}
 
-    public StatData(Building building, SyncStat<?> stat) {
-        if (building == null) return;
+    /**
+     * Store data for increasing a specific {@link Stat} for a {@link Building}.
+     * 
+     * @param building The Building whose Stat will increase.
+     * @param stat The Stat to increase.
+     */
+    public StatData(Building building, Stat stat) {
+        if (building == null || stat == null) return;
 
         mQ = building.getTile().getQ();
         mR = building.getTile().getR();
+        mStatID = stat.getID();
     }
 
     @Override
     public void serialize(DataOutputStream stream) throws IOException {
         stream.writeInt(mQ);
         stream.writeInt(mR);
+        stream.writeInt(mStatID);
     }
 
     @Override
     public void deserialize(DataInputStream stream) throws IOException {
         mQ = stream.readInt();
         mR = stream.readInt();
+        mStatID = stream.readInt();
     }
 
-    public HexagonTile getTile(HexagonMap map) {
+    /**
+     * Get the {@link HexagonTile} specified.
+     * @param map The {@link HexagonMap} being used.
+     * @return The specified {@link HexagonMap}, otherwise {@code null}.
+     */
+    private HexagonTile getTile(HexagonMap map) {
         return map.getTile(mQ, mR);
     }
 
+    /**
+     * Get the {@link Building} whose Stat is increasing.
+     * @param map The {@link HexagonMap} being used.
+     * @return The Building whose stat is increasing, or {@code null}.
+     */
     public Building getBuilding(HexagonMap map) {
         if (getTile(map) == null) return null;
         return getTile(map).getBuilding();
+    }
+    
+    /**
+     * Get the {@link Stat} that is being changed.
+     * @return The Stat, or {@code null}.
+     */
+    public Stat getStat() {
+    	return Stat.getFromID(mStatID);
     }
 }
