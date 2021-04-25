3d177c41522b46ca607fbc3ecde37873343ea0c2
Aurimas Blažulionis <0x60@pm.me>
2021-04-25 15:33:09 +0100

    Make dragging code more robust

--- game/src/main/java/org/dragonskulle/game/camera/DragMovement.java
+++ game/src/main/java/org/dragonskulle/game/camera/DragMovement.java
@@ -38,6 +38,7 @@ public class DragMovement extends Component implements IFrameUpdate, IOnAwake {
     private final Vector3f mTmpPosY = new Vector3f();
 
     private boolean mDragging;
+    private boolean mDragInvalid;
 
     @Override
     public void onAwake() {
@@ -85,14 +86,18 @@ public class DragMovement extends Component implements IFrameUpdate, IOnAwake {
                         screenPos.y(),
                         mTmpPos);
 
-        if (!mDragging
-                && cursor.hadLittleDrag()
-                && !Reference.isValid(UIManager.getInstance().getHoveredObject())) {
-            mPlanePos.set(pos);
-            mTargetHeight = height;
-            mDragging = true;
-        } else if (!Actions.TRIGGER_DRAG.isActivated()) {
+        if (!Actions.TRIGGER_DRAG.isActivated()) {
             mDragging = false;
+            mDragInvalid = false;
+        } else if (!mDragging) {
+            if (Actions.TRIGGER_DRAG.isJustActivated()
+                    && Reference.isValid(UIManager.getInstance().getHoveredObject())) {
+                mDragInvalid = true;
+            } else if (!mDragInvalid && cursor.hadLittleDrag()) {
+                mPlanePos.set(pos);
+                mTargetHeight = height;
+                mDragging = true;
+            }
         }
 
         if (!mDragging) return;
