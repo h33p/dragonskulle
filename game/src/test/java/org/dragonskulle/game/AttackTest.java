6f24d8dea2191db92ff77772be05f09edfd903ab
Oscar Lindenbaum - 2005912 <oxl912@cs.bham.ac.uk>
2021-04-26 16:42:53 +0100

    refactored test

--- game/src/test/java/org/dragonskulle/game/AttackTest.java
+++ game/src/test/java/org/dragonskulle/game/AttackTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertEquals;
 import org.dragonskulle.game.building.Building;
 import org.dragonskulle.game.building.stat.StatType;
 import org.dragonskulle.game.building.stat.SyncStat;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -15,14 +16,26 @@ import org.junit.Test;
  */
 public class AttackTest {
 
+    private Building mAttacker;
+    private Building mDefender;
+
+    @Before
+    public void setup() {
+        mAttacker = new Building();
+        mDefender = new Building();
+
+        mAttacker.onAwake();
+        mDefender.onAwake();
+    }
+
     /**
      * This will do the actual attacking
      *
-     * @param attacker The attacking {@code Building}
-     * @param defender The defending {@code Building}
+     * @param attacker The attacking {@link Building}
+     * @param defender The defending {@link Building}
      * @return The percentage of wins
      */
-    private float actualAttack(Building attacker, Building defender) {
+    private float runAttack(Building attacker, Building defender) {
         float wins = 0f;
         int iterations = 10000;
 
@@ -32,20 +45,13 @@ public class AttackTest {
             }
         }
 
-        float percentageOfWins = wins / iterations;
-        return percentageOfWins;
+        return wins / iterations;
     }
 
     /** This method tests a basic attack done by 2 buildings */
     @Test
     public void basicAttackTest() {
-        Building attacker = new Building();
-        Building defender = new Building();
-
-        attacker.onAwake();
-        defender.onAwake();
-
-        float percentageOfWins = actualAttack(attacker, defender);
+        float percentageOfWins = runAttack(mAttacker, mDefender);
 
         assertEquals(0.5f, percentageOfWins, 0.1f);
     }
@@ -56,20 +62,13 @@ public class AttackTest {
      */
     @Test
     public void attackerMaxTest() {
-
-        Building attacker = new Building();
-        Building defender = new Building();
-
-        attacker.onAwake();
-        defender.onAwake();
-
-        SyncStat stat = attacker.getStat(StatType.ATTACK);
+        SyncStat stat = mAttacker.getStat(StatType.ATTACK);
 
         stat.set(SyncStat.LEVEL_MAX);
         // Update the building on the server.
-        attacker.afterStatChange();
+        mAttacker.afterStatChange();
 
-        float percentageOfWins = actualAttack(attacker, defender);
+        float percentageOfWins = runAttack(mAttacker, mDefender);
 
         assertEquals(0.7f, percentageOfWins, 0.1f);
     }
@@ -79,19 +78,13 @@ public class AttackTest {
      */
     @Test
     public void defenderMaxTest() {
-        Building attacker = new Building();
-        Building defender = new Building();
-
-        attacker.onAwake();
-        defender.onAwake();
-
-        SyncStat stat = defender.getStat(StatType.DEFENCE);
+        SyncStat stat = mDefender.getStat(StatType.DEFENCE);
 
         stat.set(SyncStat.LEVEL_MAX);
         // Update the building on the server.
-        defender.afterStatChange();
+        mDefender.afterStatChange();
 
-        float percentageOfWins = actualAttack(attacker, defender);
+        float percentageOfWins = runAttack(mAttacker, mDefender);
 
         assertEquals(0.3f, percentageOfWins, 0.1f);
     }
@@ -102,27 +95,21 @@ public class AttackTest {
      */
     @Test
     public void bothEqualTest() {
-        Building attacker = new Building();
-        Building defender = new Building();
-
-        attacker.onAwake();
-        defender.onAwake();
-
-        float percentageOfWins = actualAttack(attacker, defender);
+        float percentageOfWins = runAttack(mAttacker, mDefender);
 
         assertEquals(0.5f, percentageOfWins, 0.1f);
 
-        SyncStat statAttacker = attacker.getStat(StatType.ATTACK);
-        SyncStat statDefender = defender.getStat(StatType.DEFENCE);
+        SyncStat statAttacker = mAttacker.getStat(StatType.ATTACK);
+        SyncStat statDefender = mDefender.getStat(StatType.DEFENCE);
         for (int i = 0; i < 6; i++) {
 
             statAttacker.increaseLevel();
             statDefender.increaseLevel();
 
-            attacker.afterStatChange();
-            defender.afterStatChange();
+            mAttacker.afterStatChange();
+            mDefender.afterStatChange();
 
-            percentageOfWins = actualAttack(attacker, defender);
+            percentageOfWins = runAttack(mAttacker, mDefender);
 
             assertEquals(0.5f, percentageOfWins, 0.1f);
         }
@@ -131,21 +118,15 @@ public class AttackTest {
     /** This is a small test when buildings are at different levels */
     @Test
     public void differentLevelsTest() {
-        Building attacker = new Building();
-        Building defender = new Building();
-
-        attacker.onAwake();
-        defender.onAwake();
-
-        SyncStat statAttacker = attacker.getStat(StatType.ATTACK);
-        SyncStat statDefender = defender.getStat(StatType.DEFENCE);
+        SyncStat statAttacker = mAttacker.getStat(StatType.ATTACK);
+        SyncStat statDefender = mDefender.getStat(StatType.DEFENCE);
 
         statAttacker.increaseLevel();
         statAttacker.increaseLevel();
 
-        attacker.afterStatChange();
+        mAttacker.afterStatChange();
 
-        float percentageOfWins = actualAttack(attacker, defender);
+        float percentageOfWins = runAttack(mAttacker, mDefender);
 
         assertEquals(0.6f, percentageOfWins, 0.1f);
 
@@ -153,9 +134,9 @@ public class AttackTest {
         statDefender.increaseLevel();
         statDefender.increaseLevel();
 
-        defender.afterStatChange();
+        mDefender.afterStatChange();
 
-        percentageOfWins = actualAttack(attacker, defender);
+        percentageOfWins = runAttack(mAttacker, mDefender);
 
         assertEquals(0.4f, percentageOfWins, 0.1f);
     }
