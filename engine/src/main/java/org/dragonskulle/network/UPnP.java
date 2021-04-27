cdda71430f7c429fedc21ee4b0134ec97787497d
Harry Stoltz <hbstoltz7@gmail.com>
2021-04-27 13:47:37 +0100

    Fix service name
    Signed-off-by: Harry Stoltz <hbstoltz7@gmail.com>

--- engine/src/main/java/org/dragonskulle/network/UPnP.java
+++ engine/src/main/java/org/dragonskulle/network/UPnP.java
@@ -41,7 +41,7 @@ public class UPnP {
                             + "HOST: 239.255.255.250:1900\r\n"
                             + "MAN: \"ssdp:discover\"\r\n"
                             + "MX: 1\r\n"
-                            + "ST: urn:schemas-upnp-org:device:WANIPConnection:1\r\n\r\n")
+                            + "ST: urn:schemas-upnp-org:service:WANIPConnection:1\r\n\r\n")
                     .getBytes();
 
     private static final String SERVICE_NAME = "urn:schemas-upnp-org:service:WANIPConnection:1";
