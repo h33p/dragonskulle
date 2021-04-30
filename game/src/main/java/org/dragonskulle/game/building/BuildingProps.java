aa61b4ab248e8494323aad95e2fe3a54eb1ced66
lxm9243 <56477797+lxm9243@users.noreply.github.com>
2021-04-30 21:27:05 +0530

    getting all the stats

--- game/src/main/java/org/dragonskulle/game/building/BuildingProps.java
+++ game/src/main/java/org/dragonskulle/game/building/BuildingProps.java
@@ -1,26 +1,43 @@
 package org.dragonskulle.game.building;
 
+import lombok.experimental.Accessors;
+import lombok.extern.java.Log;
+import org.dragonskulle.components.Component;
+import org.dragonskulle.core.Reference;
 import org.dragonskulle.game.building.stat.StatType;
 import org.dragonskulle.game.building.stat.SyncStat;
 import org.dragonskulle.game.map.HexagonTile;
 
 import java.util.Set;
+@Accessors(prefix = "m")
+@Log
+public class BuildingProps extends Component {
 
-public class BuildingProps {
-
-    private SyncStat mStat;
-
+    private SyncStat mDefenceStat;
+    private SyncStat mAttackStat;
+    private SyncStat mTokenStat;
     private Building mBuilding;
 
+    /**
+     * The base prop mesh.
+     */
+    private Reference<BuildingProps> mBuildingProps;
 
     private void onStatChange(Building building){
 
-        mStat = building.getStat(StatType.ATTACK);
 
-       Set<HexagonTile> claimedTiles = building.getClaimedTiles();
+        Set<HexagonTile> claimedTiles = building.getClaimedTiles();
+
+        mAttackStat = building.getStat(StatType.ATTACK);
+        mDefenceStat = building.getStat(StatType.DEFENCE);
+        mTokenStat = building.getStat(StatType.TOKEN_GENERATION);
 
 
 
     }
 
+    @Override
+    protected void onDestroy() {
+
+    }
 }
