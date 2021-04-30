4b2a53db2ef2c3f2aa1743fc58ec7b7c46b7ee33
craig-r-w <56520435+craig-r-w@users.noreply.github.com>
2021-04-30 17:28:41 +0100

    Added predefined buildings.

--- game/src/main/java/org/dragonskulle/game/player/PredefinedBuildings.java
+++ game/src/main/java/org/dragonskulle/game/player/PredefinedBuildings.java
@@ -14,21 +14,25 @@ import java.util.stream.Collectors;
 public class PredefinedBuildings {
     /** The Base building. */
     public static final BuildingDescriptor BASE =
-            new BuildingDescriptor(1, 1, 1, 1, 1, "ui/0_stars.png", "Stock");
+            new BuildingDescriptor(1, 1, 1, 25, 10, "ui/1_stars.png", "Camp");
     /** The predefined buildings. */
     private static final List<BuildingDescriptor> buildings =
             new ArrayList<>(
                     Arrays.asList(
                             BASE,
-                            new BuildingDescriptor(1, 1, 1, 100, 200, "ui/1_stars.png", "Oscar"),
-                            new BuildingDescriptor(2, 3, 2, 30, 999, "ui/2_stars.png", "Auri"),
-                            new BuildingDescriptor(5, 3, 4, 31, 2, "ui/3_stars.png", "Harry"),
-                            new BuildingDescriptor(8, 3, 6, 32, 2, "ui/4_stars.png", "Auri"),
-                            new BuildingDescriptor(10, 3, 8, 33, 2, "ui/5_stars.png", "Leela"),
-                            new BuildingDescriptor(4, 1, 10, 25, 2, "ui/6_stars.png", "Craig"),
-						    new BuildingDescriptor(4, 1, 10, 25, 2, "ui/6_stars.png", "Craig"),
-						    new BuildingDescriptor(4, 1, 10, 25, 2, "ui/6_stars.png", "Craig"),
-						    new BuildingDescriptor(4, 1, 10, 25, 2, "ui/9_stars.png", "Craig")));
+						    new BuildingDescriptor(3, 3, 3, 25, 20, "ui/2_stars.png", "Town"),
+						    new BuildingDescriptor(6, 6, 6, 25, 30, "ui/3_stars.png", "City"),
+                            
+                            new BuildingDescriptor(5, 3, 1, 125, 25, "ui/5_stars.png", "Barracks"),
+						    new BuildingDescriptor(3, 5, 1, 125, 25, "ui/5_stars.png", "Castle"),
+						    new BuildingDescriptor(1, 3, 5, 125, 25, "ui/5_stars.png", "Merchants"),
+                    
+						    new BuildingDescriptor(10, 8, 8, 250, 50, "ui/9_stars.png", "Military Complex"),
+						    new BuildingDescriptor(8, 10, 8, 250, 50, "ui/9_stars.png", "Fortress"),
+						    new BuildingDescriptor(8, 8, 10, 250, 50, "ui/9_stars.png", "Guild")
+            		));
+    
+    
 
     /**
      * Gets all the buildings which can be placed filtered by the cost of the building.
