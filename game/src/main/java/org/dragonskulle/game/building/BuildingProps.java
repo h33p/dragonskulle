8d588b5d670a6c05b9e520f8469e886b7851b01d
lxm9243 <56477797+lxm9243@users.noreply.github.com>
2021-04-30 23:07:48 +0530

    adds cude to all claimed tiles

--- game/src/main/java/org/dragonskulle/game/building/BuildingProps.java
+++ game/src/main/java/org/dragonskulle/game/building/BuildingProps.java
@@ -3,12 +3,16 @@ package org.dragonskulle.game.building;
 import lombok.experimental.Accessors;
 import lombok.extern.java.Log;
 import org.dragonskulle.components.Component;
+import org.dragonskulle.components.TransformHex;
+import org.dragonskulle.core.GameObject;
 import org.dragonskulle.core.Scene;
 import org.dragonskulle.game.building.stat.StatType;
 import org.dragonskulle.game.building.stat.SyncStat;
 import org.dragonskulle.game.map.HexagonMap;
 import org.dragonskulle.game.map.HexagonTile;
 import org.dragonskulle.renderer.Mesh;
+import org.dragonskulle.renderer.components.Renderable;
+import org.dragonskulle.renderer.materials.PBRMaterial;
 
 import java.util.Set;
 @Accessors(prefix = "m")
@@ -21,18 +25,26 @@ public class BuildingProps extends Component {
     private Building mBuilding;
     private HexagonMap mMap;
 
+    private static GameObject mProps = new GameObject(
+            "cube",
+            (handle) -> {
+                handle.addComponent(new Renderable(Mesh.CUBE, new PBRMaterial() ));
+            });
+
+
     private void onStatChange(Building building){
 
-        HexagonMap mMap = Scene.getActiveScene().getSingleton(HexagonMap.class);
+        mMap = Scene.getActiveScene().getSingleton(HexagonMap.class);
 
         Set<HexagonTile> claimedTiles = building.getClaimedTiles();
 
         mAttackStat = building.getStat(StatType.ATTACK);
         mDefenceStat = building.getStat(StatType.DEFENCE);
         mTokenStat = building.getStat(StatType.TOKEN_GENERATION);
-        Mesh cube = Mesh.CUBE;
 
-
+        for (HexagonTile tile : claimedTiles) {
+            mMap.getGameObject().addChild(GameObject.instantiate(mProps, new TransformHex(tile.getQ(),tile.getR())));
+        }
 
     }
 
