bbf61dc7ba11fc221f341b3a0902e10b2fa9fca7
Oscar Lindenbaum - 2005912 <oxl912@cs.bham.ac.uk>
2021-05-01 15:44:18 +0100

    defualt config values written to file

---
+++ serverless/default_config.js
@@ -0,0 +1,22 @@
+{
+    "SyncStat": {
+      "LEVEL_MIN": 1,
+      "LEVEL_MAX": 10
+    },
+    "Player": {
+      "ATTACK_COOLDOWN": 2,
+      "TOKEN_RATE": 5,
+      "TOKEN_TIME": 1
+    },
+    "ProbabilisticAiPlayer": {
+      "mBuildProbability": 0.65,
+      "mUpgradeProbability": 0.15,
+      "mAttackProbability": 0.15,
+      "mSellProbability": 0.05
+    },
+    "AiAimer": {
+      "PLAY_A_STAR": 0.9,
+      "AIM_AT_CAPITAL": 0.01,
+      "TRIES": 10
+    }
+  }
