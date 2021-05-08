02904caebe89af0285e28f6837d881109b54d85a
Nathaniel Lowis <nathaniel@lowis.co.uk>
2021-05-08 15:28:49 +0100

    Updated tests

--- game/src/test/java/org/dragonskulle/game/UpgradeTest.java
+++ game/src/test/java/org/dragonskulle/game/UpgradeTest.java
@@ -1,7 +1,7 @@
 /* (C) 2021 DragonSkulle */
 package org.dragonskulle.game;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import org.dragonskulle.game.building.Building;
@@ -15,18 +15,6 @@ import org.junit.Test;
  */
 public class UpgradeTest {
 
-    /** This checks the starting value */
-    @Test
-    public void basicTest() {
-        Building building = new Building();
-        building.onConnectedSyncvars();
-        ArrayList<SyncStat> stats = building.getUpgradeableStats();
-
-        assertEquals(4, stats.get(0).getCost());
-        assertEquals(4, stats.get(1).getCost());
-        assertEquals(4, stats.get(2).getCost());
-    }
-
     /** This will upgrade one stat to max level. */
     @Test
     public void upgradeOneStat() {
@@ -37,47 +25,56 @@ public class UpgradeTest {
         SyncStat attack = stats.get(0);
 
         // Lvl 1
-        assertEquals(1, attack.getCost());
+        int lvl1 = attack.getCost();
+
         attack.increaseLevel();
         building.afterStatChange();
 
         // Lvl 2
-        assertEquals(3, attack.getCost());
+        int lvl2 = attack.getCost();
+        assertTrue(lvl2 > lvl1);
         attack.increaseLevel();
         building.afterStatChange();
 
         // Lvl 3
-        assertEquals(6, attack.getCost());
+        int lvl3 = attack.getCost();
+        assertTrue(lvl3 > lvl2);
         attack.increaseLevel();
         building.afterStatChange();
 
         // Lvl 4;
-        assertEquals(9, attack.getCost());
+        int lvl4 = attack.getCost();
+        assertTrue(lvl4 > lvl3);
         attack.increaseLevel();
         building.afterStatChange();
 
         // Lvl 5
-        assertEquals(12, attack.getCost());
+        int lvl5 = attack.getCost();
+        assertTrue(lvl5 > lvl4);
         attack.increaseLevel();
         building.afterStatChange();
 
         // Lvl 6
-        assertEquals(15, attack.getCost());
+        int lvl6 = attack.getCost();
+        assertTrue(lvl6 > lvl5);
         attack.increaseLevel();
         building.afterStatChange();
 
         // Lvl 7
-        assertEquals(18, attack.getCost());
+        int lvl7 = attack.getCost();
+        assertTrue(lvl7 > lvl6);
         attack.increaseLevel();
         building.afterStatChange();
 
         // Lvl 8
-        assertEquals(21, attack.getCost());
+        int lvl8 = attack.getCost();
+        assertTrue(lvl8 > lvl7);
         attack.increaseLevel();
         building.afterStatChange();
 
         // Lvl 9
-        assertEquals(24, attack.getCost());
+        int lvl9 = attack.getCost();
+        assertTrue(lvl9 > lvl8);
         attack.increaseLevel();
         building.afterStatChange();
     }
@@ -93,31 +90,34 @@ public class UpgradeTest {
         SyncStat defence = stats.get(1);
         SyncStat tokenGeneration = stats.get(2);
 
-        assertEquals(1, attack.getLevel());
-        assertEquals(1, defence.getLevel());
-        assertEquals(1, tokenGeneration.getLevel());
+        int test1 = defence.getCost();
 
         // Increase attack lvl
         attack.increaseLevel();
         building.afterStatChange();
 
-        assertEquals(4, defence.getCost());
+        int test2 = defence.getCost();
+        assertTrue(test2 > test1);
 
         // Increase token generation lvl
         tokenGeneration.increaseLevel();
         building.afterStatChange();
 
-        assertEquals(8, defence.getCost());
+        int test3 = defence.getCost();
+        assertTrue(test3 > test2);
 
         // Increase attack lvl
         attack.increaseLevel();
         building.afterStatChange();
-        assertEquals(8, defence.getCost());
+        int test4 = defence.getCost();
+        assertTrue(test4 > test3);
 
         // Increase token generation lvl
         tokenGeneration.increaseLevel();
         building.afterStatChange();
 
-        assertEquals(8, defence.getCost());
+        int test5 = defence.getCost();
+
+        assertTrue(test5 > test4);
     }
 }
