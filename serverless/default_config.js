b5eea981c0cef1b949ac8cb73447069be33bdd8d
Oscar Lindenbaum - 2005912 <oxl912@cs.bham.ac.uk>
2021-05-11 15:00:55 +0100

    defqault config wasnt saved

--- serverless/default_config.js
+++ serverless/default_config.js
@@ -1,132 +1,163 @@
 {
   "global" : {
-    "inflation" : 1.001,
-    "mapSize" : 51
-  },
+  "inflation" : 1.001,
+      "mapSize" : 51
+},
   "player" : {
-    "inflationPerBuilding" : 1.04,
-    "attackCooldown" : 2.0,
-    "tokenRate" : 5.0,
-    "tokenTime" : 1.0,
-    "attackHeightMul" : 1.0
+  "inflationPerBuilding" : 1.04,
+      "attackCooldown" : 2.0,
+      "tokenRate" : 5.0,
+      "tokenTime" : 1.0,
+      "attackHeightMul" : 1.0
+},
+  "ai" : [{
+  "lowerBoundTime": 1.0,
+  "upperBoundTime": 2.0,
+  "probabilisticAi": {
+    "buildProbability": 0.6,
+    "upgradeProbability": 0.175,
+    "attackProbability": 0.22,
+    "sellProbability": 0.005
   },
-  "ai" : [ {
-    "lowerBoundTime" : 1.0,
-    "upperBoundTime" : 2.0,
-    "probabilisticAi" : {
-      "buildProbability" : 0.65,
-      "upgradeProbability" : 0.155,
-      "attackProbability" : 0.19,
-      "sellProbability" : 0.005
+  "aiAimer": {
+    "playAStar": 0.75,
+    "maxAttempts": 50,
+    "tries": 5
+  }
+},
+  {
+    "lowerBoundTime": 1.0,
+    "upperBoundTime": 2.0,
+    "probabilisticAi": {
+      "buildProbability": 0.9,
+      "upgradeProbability": 0.04,
+      "attackProbability": 0.05,
+      "sellProbability": 0.01
     },
-    "aiAimer" : {
-      "playAStar" : 0.75,
-      "maxAttempts" : 1400,
-      "tries" : 5
-    }
-    ,{
-        "lowerBoundTime" : 1.0,
-        "upperBoundTime" : 2.0,
-        "probabilisticAi" : {
-            "buildProbability" : 0.01,
-            "upgradeProbability" : 0.9,
-            "attackProbability" : 0.8,
-            "sellProbability" : 0.01
-        },
-        "aiAimer" : {
-            "playAStar" : 0.75,
-            "maxAttempts" : 50,
-            "tries" : 5
-        }
-    }],
-  "attackStat" : {
-    "value" : {
-      "baseValue" : 0.0,
-      "mulLevel" : 1.0,
-      "minValue" : 0.0,
-      "maxValue" : 100.0,
-      "bonus" : {
-        "multiplier" : 0.0,
-        "bonusTile" : null
-      }
-    },
-    "cost" : {
-      "selfLevelMultiplier" : 3.0
-    }
-  },
-  "buildDistanceStat" : {
-    "value" : {
-      "baseValue" : 2.0,
-      "mulLevel" : 0.0,
-      "minValue" : 0.0,
-      "maxValue" : 100.0,
-      "bonus" : {
-        "multiplier" : 0.0,
-        "bonusTile" : null
-      }
-    },
-    "cost" : {
-      "selfLevelMultiplier" : 3.0
-    }
-  },
-  "claimDistanceStat" : {
-    "value" : {
-      "baseValue" : 1.0,
-      "mulLevel" : 0.0,
-      "minValue" : 0.0,
-      "maxValue" : 100.0,
-      "bonus" : {
-        "multiplier" : 0.0,
-        "bonusTile" : null
-      }
-    },
-    "cost" : {
-      "selfLevelMultiplier" : 3.0
-    }
-  },
-  "defenceStat" : {
-    "value" : {
-      "baseValue" : 0.0,
-      "mulLevel" : 1.0,
-      "minValue" : 0.0,
-      "maxValue" : 100.0,
-      "bonus" : {
-        "multiplier" : 0.5,
-        "bonusTile" : "MOUNTAIN"
-      }
-    },
-    "cost" : {
-      "selfLevelMultiplier" : 3.0
-    }
-  },
-  "generationStat" : {
-    "value" : {
-      "baseValue" : -1.0,
-      "mulLevel" : 1.0,
-      "minValue" : 0.0,
-      "maxValue" : 100.0,
-      "bonus" : {
-        "multiplier" : 0.3,
-        "bonusTile" : "WATER"
-      }
-    },
-    "cost" : {
-      "selfLevelMultiplier" : 3.0
-    }
-  },
-  "viewDistanceStat" : {
-    "value" : {
-      "baseValue" : 3.0,
-      "mulLevel" : 0.0,
-      "minValue" : 0.0,
-      "maxValue" : 3.0,
-      "bonus" : {
-        "multiplier" : 0.0,
-        "bonusTile" : null
-      }
-    },
-    "cost" : {
-      "selfLevelMultiplier" : 3.0
+    "aiAimer": {
+      "playAStar": 0.75,
+      "maxAttempts": 50,
+      "tries": 5
     }
   }
+  , {
+    "lowerBoundTime": 1.0,
+    "upperBoundTime": 2.0,
+    "probabilisticAi": {
+      "buildProbability": 0.24,
+      "upgradeProbability": 0.5,
+      "attackProbability": 0.25,
+      "sellProbability": 0.01
+    },
+    "aiAimer": {
+      "playAStar": 0.75,
+      "maxAttempts": 50,
+      "tries": 5
+    }
+  }
+  , {
+    "lowerBoundTime": 1.0,
+    "upperBoundTime": 2.0,
+    "probabilisticAi": {
+      "buildProbability": 0.01,
+      "upgradeProbability": 0.9,
+      "attackProbability": 0.8,
+      "sellProbability": 0.01
+    },
+    "aiAimer": {
+      "playAStar": 0.75,
+      "maxAttempts": 50,
+      "tries": 5
+    }
+  }],
+    "attackStat" : {
+  "value" : {
+    "baseValue" : 0.0,
+        "mulLevel" : 1.0,
+        "minValue" : 0.0,
+        "maxValue" : 100.0,
+        "bonus" : {
+      "multiplier" : 0.0,
+          "bonusTile" : null
+    }
+  },
+  "cost" : {
+    "selfLevelMultiplier" : 3.0
+  }
+},
+  "buildDistanceStat" : {
+  "value" : {
+    "baseValue" : 2.0,
+        "mulLevel" : 0.0,
+        "minValue" : 0.0,
+        "maxValue" : 100.0,
+        "bonus" : {
+      "multiplier" : 0.0,
+          "bonusTile" : null
+    }
+  },
+  "cost" : {
+    "selfLevelMultiplier" : 3.0
+  }
+},
+  "claimDistanceStat" : {
+  "value" : {
+    "baseValue" : 1.0,
+        "mulLevel" : 0.0,
+        "minValue" : 0.0,
+        "maxValue" : 100.0,
+        "bonus" : {
+      "multiplier" : 0.0,
+          "bonusTile" : null
+    }
+  },
+  "cost" : {
+    "selfLevelMultiplier" : 3.0
+  }
+},
+  "defenceStat" : {
+  "value" : {
+    "baseValue" : 0.0,
+        "mulLevel" : 1.0,
+        "minValue" : 0.0,
+        "maxValue" : 100.0,
+        "bonus" : {
+      "multiplier" : 0.5,
+          "bonusTile" : "MOUNTAIN"
+    }
+  },
+  "cost" : {
+    "selfLevelMultiplier" : 3.0
+  }
+},
+  "generationStat" : {
+  "value" : {
+    "baseValue" : -1.0,
+        "mulLevel" : 1.0,
+        "minValue" : 0.0,
+        "maxValue" : 100.0,
+        "bonus" : {
+      "multiplier" : 0.3,
+          "bonusTile" : "WATER"
+    }
+  },
+  "cost" : {
+    "selfLevelMultiplier" : 3.0
+  }
+},
+  "viewDistanceStat" : {
+  "value" : {
+    "baseValue" : 3.0,
+        "mulLevel" : 0.0,
+        "minValue" : 0.0,
+        "maxValue" : 3.0,
+        "bonus" : {
+      "multiplier" : 0.0,
+          "bonusTile" : null
+    }
+  },
+  "cost" : {
+    "selfLevelMultiplier" : 3.0
+  }
+}
 }
