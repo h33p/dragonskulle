997112f8a03db22a4628f5e69c0c68158152f019
Aurimas Blažulionis <0x60@pm.me>
2021-05-13 13:50:57 +0100

    Document misc

--- game/src/main/java/org/dragonskulle/game/misc/ArcPath.java
+++ game/src/main/java/org/dragonskulle/game/misc/ArcPath.java
@@ -14,28 +14,69 @@ import org.dragonskulle.core.Reference;
 import org.joml.Vector3f;
 import org.joml.Vector3fc;
 
+/**
+ * Spawn objects in arcs.
+ *
+ * @author Aurimas Blažulionis
+ */
 @Accessors(prefix = "m")
 public class ArcPath extends Component implements IFrameUpdate {
 
+    /**
+     * Arc path handler.
+     *
+     * <p>This interface allows to modify transform of an object on arc.
+     */
     public static interface IArcHandler {
+        /**
+         * Handle arc's path.
+         *
+         * @param id index of the object.
+         * @param pathPoint point on the path the object is.
+         * @param transform transform component of the object.
+         */
         void handle(int id, float pathPoint, Transform3D transform);
     }
 
+    /** Arc path updater. */
     public static interface IPathUpdater {
+        /**
+         * Handle update.
+         *
+         * @param path path object to update.
+         */
         void handle(ArcPath path);
     }
 
+    /** Spawned arc path entry. */
     private static class SpawnedEntry {
         private final Reference<GameObject> mObject;
         private final Transform3D mTransform;
         private final float mOffset;
 
+        /**
+         * Construct a {@link SpawnedEntry}.
+         *
+         * @param object game object of this entry.
+         * @param offset current offset within the path for the object.
+         */
         public SpawnedEntry(GameObject object, float offset) {
             mObject = object.getReference();
             mTransform = object.getTransform(Transform3D.class);
             mOffset = offset;
         }
 
+        /**
+         * Update the transform of the entry.
+         *
+         * @param a start point of the path.
+         * @param b end point of the path.
+         * @param tmp temp vector for calculations.
+         * @param spawnOff spawn offset of the path.
+         * @param id id of the entry.
+         * @param amplitude amplitude of the arc.
+         * @param arcHandler handler of the arc path.
+         */
         public void updateTransform(
                 Vector3fc a,
                 Vector3fc b,
@@ -69,6 +110,7 @@ public class ArcPath extends Component implements IFrameUpdate {
             }
         }
 
+        /** Destroy this entry. */
         public void destroy() {
             if (Reference.isValid(mObject)) {
                 mObject.get().destroy();
@@ -76,52 +118,87 @@ public class ArcPath extends Component implements IFrameUpdate {
         }
     }
 
+    /** Minimum object gap to avoid infinite loops. */
     private static final float MIN_OBJ_GAP = 0.001f;
 
+    /** Template to spawn. */
     @Getter private GameObject mTemplate;
 
+    /** Currently spawned objects. */
     private final List<SpawnedEntry> mSpawnedObjects = new ArrayList<>();
 
+    /** Whether the path is dirty and should regenerate. */
     private boolean mDirty = true;
 
+    /** Gap between objects. */
     @Getter private float mObjGap = 0.1f;
 
+    /** Offset the objects on the arc. */
     @Getter @Setter private float mSpawnOffset = 0f;
 
+    /** Amplitude of the arc. */
     @Getter @Setter private float mAmplitude = 1f;
 
+    /** Arc update handler. */
     @Getter @Setter private Reference<IArcHandler> mArcHandler;
 
+    /** Object spawn starting cutoff. */
     @Getter private float mSpawnStart = 0;
+    /** Object spawn ending cutoff. */
     @Getter private float mSpawnEnd = 1;
 
+    /** Starting arc position. */
     @Getter private final Vector3f mPosStart = new Vector3f();
+    /** Target arc position. */
     @Getter private final Vector3f mPosTarget = new Vector3f();
+    /** Temporary vector. */
     private final Vector3f mTmpVec = new Vector3f();
 
+    /** Path updater. */
     @Getter @Setter private Reference<IPathUpdater> mUpdater = null;
 
+    /**
+     * Set the object template used.
+     *
+     * @param template new template to use.
+     */
     public void setTemplate(GameObject template) {
         mTemplate = template;
         setDirty();
     }
 
+    /**
+     * Set the object gap used.
+     *
+     * @param objGap new gap to use.
+     */
     public void setObjGap(float objGap) {
         mObjGap = Math.max(objGap, MIN_OBJ_GAP);
         setDirty();
     }
 
+    /**
+     * Set the path spawn start point used.
+     *
+     * @param spawnStart new value to use.
+     */
     public void setSpawnStart(float spawnStart) {
         mSpawnStart = spawnStart;
         setDirty();
     }
 
+    /**
+     * Set the path spawn end point used.
+     *
+     * @param spawnEnd new value to use.
+     */
     public void setSpawnEnd(float spawnEnd) {
         mSpawnEnd = spawnEnd;
         setDirty();
     }
 
-    public void setDirty() {
+    /** Mark the path as dirty. */
+    private void setDirty() {
         mDirty = true;
     }
 
